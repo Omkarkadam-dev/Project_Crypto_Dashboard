97f6f7f9b90fc6472e1c473e4cfcd4defccc800c
Omkar Kadam <omkarkadam1115@gmail.com>
2025-04-09 22:56:56 +0530

    add key feature section

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function typeText() {
 }
 
 window.onload = typeText;
+
+
+// Scroll Fade Animation
+const featureCards = document.querySelectorAll('[data-aos]');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+}, { threshold: 0.2 });
+
+featureCards.forEach(card => {
+  card.style.opacity = 0;
+  card.style.transform = "translateY(50px)";
+  observer.observe(card);
+});
